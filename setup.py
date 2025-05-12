91bcabf521252fddb095147003c3aa5b77c27f09
Joe Wegner <joe@wegnerdesign.com>
2025-05-12 14:38:47 -0500

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="local_ai_utils_core",
-    version="0.1.0",
+    version="0.2.0",
     packages=['local_ai_utils_core'],
     package_dir={"local_ai_utils_core": "core"},
     install_requires=[
