9d599019fbec51cea9b783810566ceb2a9a8c50c
Joe Wegner <joe@wegnerdesign.com>
2025-05-12 14:38:32 -0500

    async support and official ui support

--- core/plugin_manager.py
+++ core/plugin_manager.py
@@ -1,26 +1,31 @@
 import importlib
+import logging
+
+log = logging.getLogger(__name__)
 
 class PluginManager:
     def __init__(self, core, pluginConfig):
         self.core = core
         self.config = pluginConfig
-        self.plugins = {}
+        self._plugins = None
 
-        self.load_plugins()
+    async def plugins(self):
+        if self._plugins is None:
+            await self.load_plugins()
 
-    def getPlugins(self):
-        return self.plugins
+        return self._plugins
 
-    def load_plugins(self):
+    async def load_plugins(self):
+        self._plugins = {}
+
         for plugin_name, config in self.config.items():
             try:
                 module = importlib.import_module(f"{plugin_name}")
                 if hasattr(module, 'register'):
-                    plugin_info = module.register(self.core, config)
-                    self.plugins[plugin_name] = plugin_info
+                    plugin_info = await module.register(self.core, config)
+                    self._plugins[plugin_name] = plugin_info
                 else:
-                    print(f"Warning: Plugin {plugin_name} does not have a register function.")
+                    log.warn(f"Warning: Plugin {plugin_name} does not have a register function.")
             except ImportError as e:
-                print(f"Error: Could not import plugin {plugin_name}")
-                # Print all details
-                print(e)
+                log.warn(f"Error: Could not import plugin {plugin_name}")
+                log.warn(e)
--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     install_requires=[
         'openai',
         'pyyaml',
-        'pytest'
+        'pytest',
+        'desktop-notifier'
     ],
 )
